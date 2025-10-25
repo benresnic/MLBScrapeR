ff6c39af45312e60a4dc8203ef23a8381018c7a8
Ben Resnic <bsresnic@g.syr.edu>
2025-10-25 12:07:30 -0400

    Update imports.R

--- R/imports.R
+++ R/imports.R
@@ -1,12 +1,13 @@
 # R/imports.R
 
 #' @keywords internal
-#' @importFrom httr GET content status_code
-#' @importFrom jsonlite fromJSON
-#' @importFrom tibble tibble as_tibble
-#' @importFrom dplyr bind_rows distinct arrange transmute filter slice_head group_by
-#' @importFrom magrittr `%>%`
-#' @importFrom progress progress_bar
-#' @importFrom lubridate ymd_hms
-#' @importFrom data.table rbindlist
+#' @import tidyverse
+#' @import httr
+#' @import jsonlite
+#' @import tibble
+#' @import dplyr
+#' @import magrittr
+#' @import progress
+#' @import lubridate
+#' @import data.table
 NULL
